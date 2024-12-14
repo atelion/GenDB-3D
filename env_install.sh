f07a82d2d3ed7a9dca6b7757acf29e3c1b9c057a
atelion <happilyai.meet@gmail.com>
2024-12-14 19:29:51 +0000

    update env

--- env_install.sh
+++ env_install.sh
@@ -1,6 +1,11 @@
 # python3.9 test success
 
-pip install torch==2.1.0 torchvision==0.16.0 --index-url https://download.pytorch.org/whl/cu121
+#pip install torch==2.1.0 torchvision==0.16.0 --index-url https://download.pytorch.org/whl/cu121
+mkdir weights
+huggingface-cli download tencent/Hunyuan3D-1 --local-dir ./weights
+
+mkdir weights/hunyuanDiT
+huggingface-cli download Tencent-Hunyuan/HunyuanDiT-v1.1-Diffusers-Distilled --local-dir ./weights/hunyuanDiT
 
 pip install diffusers transformers
 
