2bb16e404f1cb2c43c765365c889dd0aa2175c0a
atelion <happilyai.meet@gmail.com>
2025-01-17 13:12:53 +0000

    add juggernautxl

---
+++ serve_juggernautxl.py
@@ -0,0 +1,49 @@
+from diffusers import DiffusionPipeline
+import torch
+from PIL import Image
+import time
+import os
+import aiohttp
+import torch
+import time
+from PIL import Image
+import argparse
+from fastapi import FastAPI, HTTPException, Body
+
+import urllib.parse
+import uvicorn
+
+from pydantic import BaseModel
+
+class RequestData(BaseModel):
+    prompt: str
+    DATA_DIR: str
+    
+app = FastAPI()
+
+pipe = DiffusionPipeline.from_pretrained("RunDiffusion/Juggernaut-XL-v9", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
+
+# prompt = "space marine locker with battle damage and identification markings."
+# prompt = "neon-lit arcade cabinet with retro graphics and worn control panel"
+# prompt = "neon cyberpunk street sign with flickering lights and holographic display"
+# extra_prompts = "Angled front view, solid color background, 3d model, detailed textures, high quality"
+# enhanced_prompt = f"{prompt}, {extra_prompts}"
+
+
+
+@app.post("/text2image")
+async def image_gen(data: RequestData):
+    print("====================================")
+    print(data)
+    output_folder = data.DATA_DIR
+    prompt = data.prompt
+
+    os.makedirs(output_folder, exist_ok=True)
+    images = pipe(prompt, num_inference_steps=50).images
+    images[0].save(os.path.join(output_folder, "img.jpg"))    
+    return {"success": True}
+if __name__ == "__main__":
+    port = 8095
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
